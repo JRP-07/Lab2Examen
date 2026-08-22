3d478e0da748ef052c9e64b86f24aebb2fa344eb
cuent <juan_valle@unitec.edu>
2026-08-22 12:00:46 -0600

    fix main

--- Lab2Examen/Examen/src/examen/Examen.java
+++ Lab2Examen/Examen/src/examen/Examen.java
@@ -1,9 +1,23 @@
 package examen;
 
+import examen.biblioteca.gui.Controlador;
+import examen.biblioteca.gui.DatosPrueba;
+import examen.biblioteca.gui.VentanaPrincipal;
+import javax.swing.SwingUtilities;
+
 public class Examen {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        Biblioteca biblioteca = new Biblioteca();
+        Controlador controlador = new Controlador(biblioteca);
+        DatosPrueba.cargar(controlador);
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                VentanaPrincipal ventana = new VentanaPrincipal(controlador);
+                ventana.setVisible(true);
+            }
+        });
     }
-    
+
 }
