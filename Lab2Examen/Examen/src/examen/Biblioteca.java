f924c9db2b8da9eb2a9eca90af07b2eebed3c43c
cuent <juan_valle@unitec.edu>
2026-08-22 11:00:26 -0600

    filtro generico por tipo y ordenaciones

--- Lab2Examen/Examen/src/examen/Biblioteca.java
+++ Lab2Examen/Examen/src/examen/Biblioteca.java
@@ -2,6 +2,7 @@ package examen;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Biblioteca {
     private ArrayList<Material> materiales;
@@ -151,6 +152,36 @@ public class Biblioteca {
             }
         }
         return null;
+    }
+
+    public <T extends Material> ArrayList<T> filtrarPorTipo(Class<T> tipo) {
+        ArrayList<T> resultado = new ArrayList<T>();
+        for (int i = 0; i < materiales.size(); i++) {
+            if (tipo.isInstance(materiales.get(i))) {
+                resultado.add(tipo.cast(materiales.get(i)));
+            }
+        }
+        return resultado;
+    }
+
+    public ArrayList<Material> ordenarPorTitulo() {
+        ArrayList<Material> copia = new ArrayList<Material>(materiales);
+        Collections.sort(copia);
+        return copia;
+    }
+
+    public ArrayList<Material> ordenarPorComplejidad() {
+        ArrayList<Material> copia = new ArrayList<Material>(materiales);
+        Collections.sort(copia, new ComparadorPorComplejidad());
+        return copia;
+    }
+
+    public ArrayList<Prestable> listarPrestables() {
+        ArrayList<Prestable> resultado = new ArrayList<Prestable>();
+        for (int i = 0; i < materiales.size(); i++) {
+            resultado.add(materiales.get(i));
+        }
+        return resultado;
     }
 
     public ArrayList<Material> getMateriales() {
