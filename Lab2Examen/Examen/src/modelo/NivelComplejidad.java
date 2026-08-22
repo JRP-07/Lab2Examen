1a66f09cfda02de69a2dfc0f0f06b479e0932a86
JoseRafael <jose_rafael2@unitec.edu>
2026-08-22 10:53:40 -0600

    Cambio minimo

--- Lab2Examen/Examen/src/modelo/NivelComplejidad.java
+++ Lab2Examen/Examen/src/modelo/NivelComplejidad.java
@@ -1,11 +1,7 @@
 package modelo;
 
 public enum NivelComplejidad {
-<<<<<<< HEAD
     BAJO(1, 0, "Contenido normal"),
-=======
-    BAJO(1, 0, "Lectura Normal"),
->>>>>>> 584eb2e7507cd556eb3f39567e3f968d42b38fc7
     MEDIO(2, 3, "Requiere conocimiento previo"),
     ALTO(3, 7, "Material especializado");
 
